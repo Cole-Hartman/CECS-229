dcf45d059a400533b253917dfd5363bca9118de9
Cole <colehartman00@gmail.com>
2024-08-29 22:55:55 -0700

    project setup

--- PA-0/sample.py
+++ PA-0/sample.py
@@ -1,15 +1,20 @@
 """PROBLEM 1"""
+
+
 def hello(subject):
-  return "Hello, " + str(subject) + "!"
+    return "Hello, " + str(subject) + "!"
+
 
 """PROBLEM 2"""
+
+
 class Bug:
-  def __init__(self, name, position = [0, 0]):
-    # todo
-    pass
+    def __init__(self, name, position=[0, 0]):
+        self.name = name
+        self.position = position
 
-  # todo: implement accessor and mutator methods
+    # todo: implement accessor and mutator methods
 
-  def __str__(self):
-    return f"Name: {self.name}\nPosition: ({self.position[0]}, {self.position[1]})"
-    
+    def __str__(self):
+        return f"Name: {self.name}\nPosition: ({self.position[0]}, {self.position[1]})"
+
