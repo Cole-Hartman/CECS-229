23159a4785200fab23a83b91c9fed403a2f1ae05
cole <colehartman00@gmail.com>
2024-09-23 21:14:41 -0700

    PA-2

--- PA-1/pa1.py
+++ PA-1/pa1.py
@@ -2,10 +2,14 @@
 
 
 def equiv_to(a, m, low, high):
-    k_low = low  # wrong; replace 'low' with the correct value
-    k_high = high  # wrong; replace 'high' the correct value
+    k_low = int((low - a) / m)
+    k_high = int((high - a) / m)
     k_vals = list(range(k_low, k_high + 1))
-    x_vals = "FIXME: initialize x_vals"
+    x_vals = []
+    for num in k_vals:
+        val = (num * m) + a
+        if val <= high and val >= low:
+            x_vals.append(val)
     return x_vals
 
 
@@ -16,13 +20,13 @@ def b_rep(n, b):
     digits = []  # stores the digits of the b-representation of n
     q = n
     while q != 0:
-        digit = "FIXME: update 'digit' to be the remainder of q divided by b"
+        digit = q % b
         if b == 16 and digit > 9:
-            hex_dict = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
-            # FIXME: Update digit = ...
-        digits.append(digit)
-        q = "FIXME: update q to the correct value."
-    return  # FIXME: Return the string of digits
+            hex_dict = {10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F"}
+            digit = hex_dict[digit]
+        digits.append(str(digit))
+        q = q // b
+    return "".join(digits[::-1])
 
 
 """ ---------------- PROBLEM 3 ----------------"""
@@ -30,8 +34,8 @@ def b_rep(n, b):
 
 def binary_add(a, b):
     # removing all whitespace from the strings
-    a = a.replace(' ', '')
-    b = b.replace(' ', '')
+    a = a.replace(" ", "")
+    b = b.replace(" ", "")
 
     # padding the strings with 0's so they are the same length
     if len(a) < len(b):
@@ -49,10 +53,12 @@ def binary_add(a, b):
         b_i = int(b[i])
 
         # FIXME: Update result += ....
+        result += str((a_i + b_i + carry) % 2)
         # FIXME: Update carry =
+        carry = (a_i + b_i + carry) // 2
     if carry == 1:
-        result += "FIXME: Update 'result' to the correct value."
-    return  # FIXME return the appropriate string
+        result += str(carry)  # "FIXME: Update 'result' to the correct value."
+    return result[::-1]  # FIXME return the appropriate string
 
 
 """ ---------------- PROBLEM 4 ----------------"""
@@ -60,19 +66,16 @@ def binary_add(a, b):
 
 def binary_mul(a, b):
     # removing all whitespace from the strings
-    a = a.replace(' ', '')
-    b = b.replace(' ', '')
-
-    # multiplication algorithm
+    a = a.replace(" ", "")
+    b = b.replace(" ", "")
     partial_products = []
     i = 0  # index of the current bit of string 'a' beginning at 0, right-to-left
     for bit in reversed(a):
-        if bit == '1':
-            partial_products.append("FIXME: Append the appropriate partial product")
+        if bit == "1":
+            partial_products.append(b + "0" * i)
         i += 1
-
-    result = '0'
+    result = "0"
     while len(partial_products) > 0:
-        result = binary_add("FIXME: Input the correct arguments")
+        result = binary_add(result, partial_products[0])
         del partial_products[0]
-    return  # FIXME: Return the appropriate result
+    return result
